130fa07ddf1529b262f8d30d2d5bfdb9402810e7
Maksym Humeniuk <maizeproduction@gmail.com>
2020-11-21 12:46:42 +0200

    finishing homework

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
 import galleryItems from './gallery-items.js'
 
+
+
 const galleryRef = document.querySelector('.js-gallery');
 const modalRef = document.querySelector('.js-lightbox');
-const imageRef = document.querySelector('.lightbox__image');
 const closeBtnRef = document.querySelector('button[data-action="close-lightbox"]');
+const imageInModal = document.querySelector('.lightbox__image');
+
+document.addEventListener('click', onImageClick);
+closeBtnRef.addEventListener('click', closingClick);
+modalRef.firstElementChild.addEventListener('click', closingClick);
+document.addEventListener('keydown', onEscClick);
+document.addEventListener('keydown', onArrowTap);
+
+
+createImageList(galleryItems)
 
 
 function createImageList(arr) {
 
-    arr.forEach(obj => {
+    arr.forEach((obj, index) => {
         const { preview, original, description } = obj
 
         const galleryItem = document.createElement('li');
@@ -23,6 +34,7 @@ function createImageList(arr) {
         galleryImage.setAttribute('src', preview);
         galleryImage.setAttribute('data-source', original);
         galleryImage.setAttribute('alt', description);
+        galleryImage.setAttribute('data-index', index);
 
         galleryLink.appendChild(galleryImage)
         galleryItem.appendChild(galleryLink)
@@ -32,52 +44,58 @@ function createImageList(arr) {
     )
 }
 
+function onArrowTap(e) {
+    let imageIndex = Number(imageInModal.dataset.index)
 
-createImageList(galleryItems)
-
-
-document.addEventListener('click', onImageClick);
-document.addEventListener('click', onCloseBtnClick);
-document.addEventListener('click', onOverlayClick);
-
+    if (e.code === "ArrowRight") {
+        if (imageIndex + 1 > galleryItems.length - 1) {
+            return
+        }
+        imageIndex +=1
+        imageInModal.src = galleryItems[imageIndex].original;
+        imageInModal.alt = galleryItems[imageIndex].description;
+        imageInModal.dataset.index = imageIndex
+    } else if (e.code === "ArrowLeft") {
+        if (imageIndex - 1 < 0) {
+            return
+        }
+        imageIndex -=1
+        imageInModal.src = galleryItems[imageIndex].original;
+        imageInModal.alt = galleryItems[imageIndex].description;
+        imageInModal.dataset.index = imageIndex
+    }
+}
 
 function onImageClick(e) {
     const href = document.querySelector('.gallery__link');
 
-    const imagePreview = e.target
-        if (imagePreview.nodeName !== 'IMG') {
+       const imagePreview = e.target
+    if (imagePreview.className !== 'gallery__image') {
         return
     } else if (imagePreview.nodeName !== 'A') {
         e.preventDefault()
     }
-    imageRef.setAttribute('src', imagePreview.dataset.source)
+
+    imageInModal.setAttribute('src', imagePreview.dataset.source)
+    imageInModal.setAttribute('alt', imagePreview.alt)
+    imageInModal.setAttribute('data-index', imagePreview.dataset.index)
+
+
     modalRef.classList.add('is-open')
 
-
 }
 
-function onCloseBtnClick(e) {
-    const closeButton = e.target
-    
-    if (closeButton.nodeName !== 'BUTTON' && closeButton.dataset !== "close-lightbox" ) {
-        return
-    }
-
+function closingClick(e) {
     modalRef.classList.remove('is-open')
-    
 }
 
-
-    function onOverlayClick(e) {
-        const overlay = e.target
-
-        if (overlay.className !== 'lightbox__overlay') {
-            return
-        }
-        
-        modalRef.classList.remove('is-open')
-
+function onEscClick(e) {
+    if (!modalRef.classList.contains('is-open')) {
+        return
+    } else if (e.code === 'Escape') {
+        modalRef.classList.remove('is-open')  
+    } 
+    return
     }
-
 
 
